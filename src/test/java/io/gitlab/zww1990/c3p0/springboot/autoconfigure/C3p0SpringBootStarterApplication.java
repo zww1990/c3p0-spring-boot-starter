60fa1d6724aa06f7ab5dd3e2d6813dfa50962f1e
zww1990 <zww1990@foxmail.com>
2024-02-16 14:26:22 +0800

    添加单元测试

---
+++ src/test/java/io/gitlab/zww1990/c3p0/springboot/autoconfigure/C3p0SpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package io.gitlab.zww1990.c3p0.springboot.autoconfigure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C3p0SpringBootStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C3p0SpringBootStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/gitlab/zww1990/c3p0/springboot/autoconfigure/C3p0SpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package io.gitlab.zww1990.c3p0.springboot.autoconfigure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class C3p0SpringBootStarterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
