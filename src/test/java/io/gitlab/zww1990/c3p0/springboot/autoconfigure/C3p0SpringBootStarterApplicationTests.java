843290a0628e1a19bd58ee1248c5d9151b839063
zww1990 <zww1990@foxmail.com>
2024-02-19 14:27:50 +0800

    添加单元测试

--- src/test/java/io/gitlab/zww1990/c3p0/springboot/autoconfigure/C3p0SpringBootStarterApplicationTests.java
+++ src/test/java/io/gitlab/zww1990/c3p0/springboot/autoconfigure/C3p0SpringBootStarterApplicationTests.java
@@ -1,13 +1,24 @@
 package io.gitlab.zww1990.c3p0.springboot.autoconfigure;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.time.LocalDateTime;
 
 @SpringBootTest
 public class C3p0SpringBootStarterApplicationTests {
+	@Autowired
+	private ApplicationContext context;
 
 	@Test
-	public void contextLoads() {
+	public void testContextLoads() {
+		C3p0DataSourceAutoConfigure configure = context.getBean(C3p0DataSourceAutoConfigure.class);
+		System.err.println(configure.getClass());
+		JdbcTemplate jdbcTemplate = context.getBean(JdbcTemplate.class);
+		System.err.println(jdbcTemplate.queryForObject("select now()", LocalDateTime.class));
 	}
 
 }
